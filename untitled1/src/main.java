b926225df60ec3c2a2417edef189a0781341a121
Francisco Almeida <101333985+Gaalfr@users.noreply.github.com>
2026-04-07 11:50:19 -0300

    Add files via upload

---
+++ untitled1/src/main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList
+//sistema de gestao de financias pessoais
+public class main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+while (true){
+
+    System.out.println("entre com sua receita (salario ou lucros):-------> 1");
+    System.out.println("entre com suas despesas:-------------------------> 2");
+    System.out.println("ver saldo:---------------------------------------> 3");
+    System.out.println("mostrar extrato:---------------------------------> 4");
+
+    Integer select = scanner.nextInt();
+
+    switch (select){
+
+        case 1:
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+    }
+
+
+
+}
+
