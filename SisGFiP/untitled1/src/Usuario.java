7d0fb1002316081bfbdb7dd1181d4ef27313999d
Francisco Almeida <101333985+Gaalfr@users.noreply.github.com>
2026-04-12 10:31:09 -0300

    Add files via upload
    -adiciona interface gráfica com Swing
    - cria TelaLogin com autenticação e cadastro de usuários
    - cria TelaPrincipal com operações financeiras
    - integra Usuario, Conta e Movimentacao à interface
    - substitui interação via console por GUI baseada em eventos

--- SisGFiP/untitled1/src/Usuario.java
+++ SisGFiP/untitled1/src/Usuario.java
@@ -1,21 +1,26 @@
 public class Usuario {
     private String nomeUsuario;
     private String senha;
+    private Conta conta;
 
-
-    public Usuario(String nomeUsuario, String senha){
-
+    public Usuario(String nomeUsuario, String senha) {
         this.nomeUsuario = nomeUsuario;
-        this.senha= senha;}
+        this.senha = senha;
+        this.conta = new Conta();
+    }
 
+    public boolean autenticar(String usuarioDigitado, String senhaDigitada) {
+        return nomeUsuario.equals(usuarioDigitado) && senha.equals(senhaDigitada);
+    }
 
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
 
-
-
-public boolean autenticar(String usuarioDigitado, String senhaDigitada) {
-    return nomeUsuario.equals(usuarioDigitado) && senha.equals(senhaDigitada);
-}}
-
+    public Conta getConta() {
+        return conta;
+    }
+}
 
 
 
